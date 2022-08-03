2373e5e6d8d804caefbc8d0e8aefe559a0801a73
boris-ti <mgbit3214@gmail.com>
2022-08-03 13:27:56 +0300

    some unpates

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -14,7 +14,7 @@ const prodConfig = {
             name: 'auth',
             filename: 'remoteEntry.js',
             exposes: {
-                './AurhApp': './src/bootstrap',
+                './AuthApp': './src/bootstrap',
             },
             shared: packageJson.dependencies,
         }),
